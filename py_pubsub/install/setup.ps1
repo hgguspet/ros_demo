8ac3b2da969538a2f5e7747a7ca909c37b87f817
hgguspet <gustav.pettersson.bjorklund@hitachigymnasiet.se>
2025-09-26 15:31:29 +0200

    py_pubsub

--- py_pubsub/install/setup.ps1
+++ py_pubsub/install/setup.ps1
@@ -23,6 +23,7 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/fireproof/Documents/ros_demo/one_axis_robot/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
